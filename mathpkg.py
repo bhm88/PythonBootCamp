507a329ac71092915808fca26bfec1480d861c2b
bhm88 <bharathph@yahoo.in>
2020-09-12 22:55:38 +0530

    test

---
+++ mathpkg.py
@@ -0,0 +1,19 @@
+import math
+
+print("ceil(4.4) ",math.ceil(4.4))
+
+print("floor(4.4) ",math.floor(4.4))
+
+print("fabs(-4.4) ",math.fabs(-4.4))
+
+print("trunc(4.4) ",math.trunc(4.4))
+
+print("factorial(4) ",math.factorial(4))
+
+print("pow(4,4) ",math.pow(4,4))
+
+print("sqrt(25) ",math.sqrt(25))
+
+print("e(4.4) ",math.e)
+
+print("pi(4.4) ",math.pi)
